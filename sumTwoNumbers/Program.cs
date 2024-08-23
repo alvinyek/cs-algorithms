8f788e8b40d50a30f849f968badefc758c8ebd63
ElvinYekayev <elvinyeka@gmail.com>
2024-08-23 23:27:32 +0400

    add two numbers

---
+++ sumTwoNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace sumTwoNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // first solution
+            //Console.Write("Enter a first number: ");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.Write("Enter a second number: ");
+            //int b = int.Parse(Console.ReadLine());
+
+            //int sum = a + b;
+            int sum = Sum();
+            Console.WriteLine("The sum is: " + sum);
+        }
+
+        static private int Sum  ()
+        {
+            Console.Write("Enter a first number: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Enter a second number: ");
+            int b = int.Parse(Console.ReadLine());
+            return a + b;
+        }
+    }
+}
